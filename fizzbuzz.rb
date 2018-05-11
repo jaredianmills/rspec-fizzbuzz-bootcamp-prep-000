2445a76cfeafd44e29b01c0f7beda623e63cdcc2
jaredianmills <jaredianmills@gmail.com>
2018-05-11 20:28:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
-    elsif int % 5 == 0
-      "Buzz"
-    end
-    elsif int % 3 == 0 && int % 5 == 0
-      "FizzBuzz"
-    end
+  if int % 5 == 0
+    "Buzz"
+  end
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  end
 end
